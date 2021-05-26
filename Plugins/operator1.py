8d24c1660512400798ffe49a4d56d9b7f3458f29
murilo-simio <murilo2110@hotmail.com>
2021-05-26 11:47:41 -0300

    User can now create his own plugin

---
+++ Plugins/operator1.py
@@ -0,0 +1,5 @@
+class Plugin:
+    def process(self, num1, num2):
+        operator_list = ['>', '<']
+        print(operator_list)
+        return operator_list
--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+from os import listdir
+from os.path import isfile, join
 from core import MyApplication
 
-myPlugins = ["plugin1", "default"]
+files = [f for f in listdir('Plugins') if isfile(join('Plugins', f))]
+myPlugins = [f.split('.', 1)[0] for f in files]
 
 if __name__ == "__main__":
     app = MyApplication(myPlugins)
